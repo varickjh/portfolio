589fdff00d855c8a2cdcbd5dd127f6a499b5b034
Varick Janiro Hasim <140166742+varickjh@users.noreply.github.com>
2025-04-25 14:41:02 -0700

    lab4

--- global.js
+++ global.js
@@ -88,3 +88,37 @@ if ("colorScheme" in localStorage){
     document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
     select.value = localStorage.colorScheme;
 }
+
+// Lab 4
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';
+  for (const proj of project){
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${proj.title}</${headingLevel}>
+      <img src="${proj.image}" alt="${project.title}">
+      <p>${proj.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
